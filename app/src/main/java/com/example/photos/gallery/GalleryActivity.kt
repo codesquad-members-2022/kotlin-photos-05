35a9bd6a9706e2133af478323def206d1c602802
banjjak2 <interr0bang@naver.com>
2022-03-22 14:33:00 +0900

    feat: AppBar 구현 및 타이틀 지정

---
+++ app/src/main/java/com/example/photos/gallery/GalleryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.photos.gallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.photos.R
+
+class GalleryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gallery)
+    }
+}
